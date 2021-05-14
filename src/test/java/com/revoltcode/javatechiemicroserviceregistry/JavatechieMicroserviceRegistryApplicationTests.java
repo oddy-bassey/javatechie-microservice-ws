ab79bab8a5ce81da4d19bf53525e86203d703aab
@revoltcode <oddybassey@gmail.com>
2021-05-14 08:13:52 +0100

    setup eureka service registry. closes #1

---
+++ src/test/java/com/revoltcode/javatechiemicroserviceregistry/JavatechieMicroserviceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.javatechiemicroserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatechieMicroserviceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
